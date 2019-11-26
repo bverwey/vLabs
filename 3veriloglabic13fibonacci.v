3775654b81bb55e8ed1af0deb17f5dc2aed918c1
bverwey <57469215+bverwey@users.noreply.github.com>
2019-11-25 18:51:43 -0800

    fib complete

--- 3veriloglabic13fibonacci.v
+++ 3veriloglabic13fibonacci.v
@@ -1,15 +1,25 @@
-module fib(
-  input clk,
-  output reg [3:0] y
-) ;
-  
-  always @(posedge clk) begin
-   begin
-    y=0; 
-     for (int i = 0; i++) begin
-    end
-endmodule
+module fib(clk, y);
+  input clk;
+  output reg [3:0] y;
 
-//one clock one output y that is a register output of four bits 3:0
-//3 bit register output
-//input clk
+  reg [3:0] prev;
+  
+  initial begin
+    prev = 1;
+    y = 0;
+  end
+  
+  always@(posedge clk) begin
+    if(y>= 13)
+    begin 
+    prev <=1;
+    y <=0;
+          end
+          else
+            begin 
+            y <= y + prev;
+            prev <= y;
+  	  end
+  end
+  
+endmodule 
