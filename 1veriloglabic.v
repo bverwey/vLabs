ccac65d243553b4ecb67dc6b7a8f4f02400d09ea
bverwey <57469215+bverwey@users.noreply.github.com>
2019-12-06 16:04:37 -0800

    126

--- 1veriloglabic.v
+++ 1veriloglabic.v
@@ -1,22 +1,11 @@
-module and_gate(a, b, c, y);
-  input a, b, c; // input ports
-  output y;    // output ports...
-  //putting a and b and gate into one output
-  // and gate1(y, a, b); instead of assign statement
-  assign y = a & ~ b | a & ~ c; // (a AND b)
- 
+module lab1(a,b,c,y);
+
+input a,b,c;
+  output y;
+
+  assign y = (a & (~b)) | (a & (~c));
+
 endmodule
-  
-  
-  
-  
-  module and_gate_top(
- output y
-);
-    and_gate uut(1, 1, 1, y);
-    //put in variables 
-    
-    endmodule
 
 
 
